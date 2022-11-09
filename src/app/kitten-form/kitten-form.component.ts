e618ee800fa9fb5d01b1cd0d0a4755407c767fc0
Boumedienne-dev <bmessadia83@gmail.com>
2022-11-09 14:37:11 +0100

    quete 09

---
+++ src/app/kitten-form/kitten-form.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kitten-form',
+  templateUrl: './kitten-form.component.html',
+  styleUrls: ['./kitten-form.component.css']
+})
+export class KittenFormComponent implements OnInit {
+createKitten() {
+throw new Error('Method not implemented.');
+}
+kittenForm: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
